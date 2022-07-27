ddc39d47da67cf9e058da60983f319261d11ab08
robyn anderson <robynjanders@gmail.com>
2022-07-27 14:41:22 -0400

    Adjusted title and favicon.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './Footer.css'
 import linkedInLogo from '../../assets/In-White-21.png'
 import gitHubLogo from '../../assets/GitHub-Mark-Light-32px.png'
 
 
 function Footer() {
-// Location, copyright, links to gitHub and LinkedIn  
+  // Location, copyright, links to gitHub and LinkedIn  
 
   return (
-   
+
     <footer>
       <div className='footer-text'>
         <em>Located in Stowe, Vermont</em>
       </div>
       <div>
-        <a href='https://www.linkedin.com/in/robyn-anderson-88b80242' target='_blank'><img src={linkedInLogo} alt='linkedIn logo link'/></a><a href='https://github.com/robyn-anderson' target='_blank'><img width='21px'src={gitHubLogo} alt='linkedIn logo link'/></a>
+        <a href='https://www.linkedin.com/in/robyn-anderson-88b80242' target='_blank'><img src={linkedInLogo} alt='linkedIn logo link' /></a><a href='https://github.com/robyn-anderson' target='_blank'><img width='21px' src={gitHubLogo} alt='linkedIn logo link' /></a>
       </div>
       <div className='footer-text'>
         <em>© 2022 Robyn Anderson</em>
