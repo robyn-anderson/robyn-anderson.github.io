96b25290d9b4c33935674406a6e6057d42771b49
robyn-anderson <106846264+robyn-anderson@users.noreply.github.com>
2022-07-29 16:40:38 -0400

    Added animation using react-spring on the interests page. Started working on emailjs for contact form.

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,27 +1,37 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser';
 import './ContactForm.css'
 
 function ContactForm(props) {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_s3i1q6a', 'template_z8422j9', form.current, '_6-_odruM0cyRBlx3')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
   // Contact form is a modal. So far it does not 'do' anything (no email sent). Plan to add this later, possibly using EmailJS?
-    if (props.form === true) {
-        return (
-          <main id='form-background'>
-            <form id='form-content'>
-                <label htmlFor='name'>Your Name</label>
-                <input type='text' />
-                <label htmlFor='email'>Email</label>
-                <input type='email' />
-                <label htmlFor='phone'>Phone</label>
-                <input type='phone' />
-                <input type='submit' className='form-submit' onClick={props.handleClick} /> {/* Close the form on 'submit'. Should add a way to display a quick 'thank you' - maybe using a timeout function and naother modal? */}
-            </form>
-          </main>
-        );
-        }
-    
-        return (
-          <div style={{display:'none'}}></div>
-        );
+  if (props.form === true) {
+    return (
+      <main id='form-background'>
+        <form ref={form} onSubmit={sendEmail} id='form-content'>
+          <input type='text' placeholder="Your Name" />
+          <input type='email' placeholder="Email" />
+          <input type='phone' placeholder="Phone" />
+          <input type='submit' className='form-submit' onClick={props.handleClick} /> {/* Close the form on 'submit'. Should add a way to display a quick 'thank you' - maybe using a timeout function and naother modal? */}
+        </form>
+      </main>
+    );
+  }
+
+  return (
+    <div style={{ display: 'none' }}></div>
+  );
 }
 
 export default ContactForm
--- src/components/Interests/Interests.jsx
+++ src/components/Interests/Interests.jsx
@@ -1,25 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useTransition, animated } from 'react-spring'
 import './Interests.css'
 import skiingImg from '../../assets/skiing.jpeg'
 import hikingImg from '../../assets/hiking.jpeg'
-import knittingImg from '../../assets/knitting.jpeg'
 import bakingImg from '../../assets/baking.jpeg'
 import bikingImg from '../../assets/biking.jpeg'
 import hikingImg2 from '../../assets/hiking2.jpeg'
 
+const interestImgs = [ // Making an array of the images for the animation
+  skiingImg,
+  hikingImg,
+  bakingImg,
+  bikingImg,
+  hikingImg2
+]
+
 function Interests() {
-  // Images showing interests
+
+  // Position in the array starts at 0
+  const [position, setPosition] = useState(0)
+
+  // React-spring useTransitions to swap out images (ref: https://react-spring.dev/hooks/use-transition#usetransition)
+  const transitions = useTransition(position, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+  })
+
+  // UseEffect and setInterval to run the animation
+  useEffect(() => {
+    let imgInterval = setInterval(() => {
+      setPosition((index) => (index + 1) % interestImgs.length)
+    }, 3000)
+    return () => clearInterval(imgInterval) // Clear the interval so it doesn't keep speeding up over time
+  }, [])
+
   return (
     <>
-        <h1><em>Interests</em></h1>
-        <div className='interest-images'>
-          <img src={hikingImg2} alt='Hiking with my dog' />
-          <img src={knittingImg} alt='Knitting socks' />
-          <img src={skiingImg} alt='Backcountry skiing' />
-          <img src={hikingImg} alt='Relaxing in the outdoors' />
-          <img src={bakingImg} alt='Baking tasty treats' />
-          <img src={bikingImg} alt='All kinds of biking' />
-        </div>
+      <h1><em>Interests</em></h1>
+      <div className='interest-images'>
+        {transitions((style, index) => (
+          // animate the figure for the swapping images
+          <animated.figure>
+            <img src={interestImgs[index]} alt="Interests" />
+          </animated.figure>
+        ))}
+      </div>
     </>
   )
 }
